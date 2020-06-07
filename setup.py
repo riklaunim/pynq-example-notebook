143bd60618f182cec6d2980ccfa017abf8ba8318
riklaunim <riklaunim@gmail.com>
2020-06-07 19:29:56 +0200

    Rename folder.

--- setup.py
+++ setup.py
@@ -6,9 +6,13 @@ from setuptools import setup, find_packages
 
 package_name = 'example-pynq-notebook'
 
+notebook_source_folder = 'notebook/'
+board_notebooks_dir = os.environ['PYNQ_JUPYTER_NOTEBOOKS']
+
+
 setup(
     name=package_name,
-    version='0.3',
+    version='0.5',
     description='Just an example notebook',
     author='PYNQ Hero',
     url='https://github.com/riklaunim/pynq-example-notebook',
@@ -21,12 +25,10 @@ setup(
 
 
 def install_notebook(notebook_name):
-    board_notebooks_dir = os.environ['PYNQ_JUPYTER_NOTEBOOKS']
-
     notebook_path = os.path.join(board_notebooks_dir, notebook_name)
     if os.path.isdir(notebook_path):
         shutil.rmtree(notebook_path)
-    shutil.copytree("example/", notebook_path)
+    shutil.copytree(notebook_source_folder, notebook_path)
 
 
 if 'install' in sys.argv:
